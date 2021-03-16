935681d2f64d9650ba6c72af232de371ae97aa70
takehiro iino <50320445+jin237@users.noreply.github.com>
2021-03-16 12:58:52 +0900

    Add files via upload

---
+++ try_3/lesson1/dbconnect.php
@@ -0,0 +1,31 @@
+<?php
+require_once './env.php';
+function connect()
+{
+    // 変数として、DBの各要素
+    $host = DB_HOST;
+    $db   = DB_NAME;
+    $user = DB_USER;
+    $pass = DB_PASS;
+
+    // どこにおくのか、名前は何か、文字はどれか
+    $dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
+
+    // DBへの接続
+    try {
+        $pdo = new PDO($dsn, $user, $pass, [
+            PDO::ATTR_ERRMODE =>PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE =>PDO::FETCH_ASSOC
+        ]);
+        // 成功したら表示
+        echo 'Connect Succeess!';
+    } catch(PDOExeption $e) {
+        // 失敗したら表示
+        echo 'Connect Failued.'. $e->getMessage();
+        exit();
+    }
+
+
+}
+
+echo connect();
