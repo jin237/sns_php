9c00c7ff343ce1f16144b0bcccdfb373c2f2de08
takehiro iino <50320445+jin237@users.noreply.github.com>
2021-03-20 21:20:02 +0900

    Add files via upload

---
+++ try_3/lesson5/env.php
@@ -0,0 +1,6 @@
+<?php
+// ホストと名前、ユーザ、パスワードの環境設定
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'user');
+define('DB_USER', 'jin');
+define('DB_PASS', 'jin237');
---
+++ try_3/lesson5/public/signup_form.php
@@ -0,0 +1,33 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>user regist page</title>
+</head>
+<body>
+    <h2>USER REGIST FORM</h2>
+    <form action="register.php" method="POST">
+    <p>
+        <label for="username"> Please enter "User name":</label>
+        <input type="text" name="username">
+    </p>
+    <p>
+        <label for="email">  Please enter "E-mail":</label>
+        <input type="email" name="email">
+    </p>
+    <p>
+        <label for="password">  Please enter "Password":</label>
+        <input type="password" name="password">
+    </p>
+    <p>
+        <label for="password_conf">  Please enter "Password (again)":</label>
+        <input type="password" name="password_conf">
+    </p>
+    <input type="submit" value="NEW REGISTRAION">
+    </form>
+</body>
+</html>
